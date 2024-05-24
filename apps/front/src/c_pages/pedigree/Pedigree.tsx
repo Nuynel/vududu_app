f8090ea8300211573cc0778a8dc1524e711214f0
alisa_kislova <alisa.k@plyo.io>
2024-05-24 12:59:35 +0200

    refactoring: added proband select

--- apps/front/src/c_pages/pedigree/Pedigree.tsx
+++ apps/front/src/c_pages/pedigree/Pedigree.tsx
@@ -10,6 +10,7 @@ import {CommonField, LinkedField} from "../../g_shared/ui_components";
 import {useLocation} from "wouter";
 import {getRuTranslate} from "../../g_shared/constants/translates";
 import * as React from "react";
+import ProbandSelect from "./ui/ProbandSelect";
 
 // на десктопе должна быть колонка слева, где отображается информация по собакам,
 // предтсавленным в родословной (по дефолту пробанд)
@@ -18,30 +19,6 @@ import * as React from "react";
 // информация о собаке и появляется кнопка "Перейти в карточку собаки"
 
 // на мобильном основная информация и кнопка выводятся снизу
-
-// type PedigreeNodes = {[key: number]: { id: string, fullName: string }[]}
-
-// function traverseTreeBFS(root: Pedigree): PedigreeNodes {
-//   const result: PedigreeNodes = {};
-//   const queue: { node: Pedigree; level: number }[] = [{ node: root, level: 0 }];
-//
-//   while (queue.length > 0) {
-//     const { node, level } = queue.shift()!;
-//
-//     if (!result[level]) {
-//       result[level] = [];
-//     }
-//     result[level].push({id: node._id, fullName: node.fullName});
-//
-//     if (node.father) {
-//       queue.push({ node: node.father, level: level + 1 });
-//     }
-//     if (node.mother) {
-//       queue.push({ node: node.mother, level: level + 1 });
-//     }
-//   }
-//   return result;
-// }
 
 type PedigreeNodes = {id: string, fullName: string, position: string}[]
 
@@ -58,28 +35,15 @@ function traverseTreeDFS(node: Pedigree, results: PedigreeNodes = []): PedigreeN
 }
 
 const PedigreeScreen = () => {
-  const {isSmall} = useResponsiveGrid(true);
-  const [dogsList, changeDogsList] = useState<DogData[]>([])
   const [probandId, changeProbandId] = useState<string | null>(null)
   const [nodes, changeNodes] = useState<PedigreeNodes>([])
-  // const containerRef = useRef(null);
-  const {dogsData, getDogById} = useProfileDataStore();
+  const {getDogById} = useProfileDataStore();
   const [, setLocation] = useLocation();
-
-  const getDogs = (searchString: string) => {
-    return dogsData.filter(dogData => dogData.fullName.toLowerCase().includes(searchString.toLowerCase()) || dogData.name.toLowerCase().includes(searchString.toLowerCase()))
-  }
-
-  const handleSearch = (searchString: string) => changeDogsList(getDogs(searchString))
 
   const getCardsConfig = (): FieldData[] => {
     const dog = getDogById(probandId);
     return dogShortDataFields.map(fieldName => getCommonFieldsConfig(fieldName, dog))
   }
-
-  useEffect(() => {
-    changeDogsList(dogsData)
-  }, [dogsData])
 
   useEffect(() => {
     if (probandId) {
@@ -100,33 +64,8 @@ const PedigreeScreen = () => {
       ]}
       height={'100%'}
     >
-      <Box
-        gridArea={'mainFilter'}
-        alignSelf='center'
-        justify={isSmall ? 'around' : 'end'}
-        background={'white'}
-        pad={'small'}
-        // direction={'row'}
-        // height={isSmall ? 'auto' : '70px'}
-      >
-        <Box>
-          <Text size={'medium'} margin={{right: 'large', top: 'medium', bottom: 'small'}}>
-            Пробанд
-          </Text>
-        </Box>
-        <FormField htmlFor='filter-input-id' margin={{top: 'small'}}>
-          <Select
-            id='filter-input-id'
-            name='Фильтр'
-            value={dogsList.find((dogData) => dogData._id === probandId)}
-            options={dogsList}
-            labelKey='fullName'
-            placeholder='Фильтр'
-            onSearch={(searchString) => handleSearch(searchString)}
-            onChange={(event) => changeProbandId(event.value._id)}
-          />
-        </FormField>
-      </Box>
+      <ProbandSelect probandId={probandId} changeProbandId={changeProbandId}/>
+
       <Box
         gridArea={'secondaryFilter'}
         direction={"row"}
@@ -135,7 +74,6 @@ const PedigreeScreen = () => {
         <Box height={"small"} width={"small"} background={'#777777'} margin={{right: 'small'}}/>
         <Box justify={"around"}>
           {probandId && getCardsConfig().map((field, index) => {
-              // return <CommonField key={index} fieldName={field.key} fieldValue={field.value} />
               return (
                 <Grid
                   columns={['1fr', '1fr']}
---
+++ apps/front/src/c_pages/pedigree/ui/ProbandSelect.tsx
@@ -0,0 +1,56 @@
+import {Box, FormField, Select, Text} from "grommet";
+import * as React from "react";
+import useResponsiveGrid from "../../../f_entities/hooks/useResponsiveGrid";
+import {DogData} from "../../../g_shared/types";
+import {useEffect, useState} from "react";
+import {useProfileDataStore} from "../../../f_entities/store/useProfileDataStore";
+
+type Props = {
+  probandId: string,
+  changeProbandId: (id: string) => void,
+}
+
+const ProbandSelect = ({probandId, changeProbandId}: Props) => {
+  const {isSmall} = useResponsiveGrid(true);
+  const [dogsList, changeDogsList] = useState<DogData[]>([])
+  const {dogsData} = useProfileDataStore();
+
+  const getDogs = (searchString: string) => {
+    return dogsData.filter(dogData => dogData.fullName.toLowerCase().includes(searchString.toLowerCase()) || dogData.name.toLowerCase().includes(searchString.toLowerCase()))
+  }
+  const handleSearch = (searchString: string) => changeDogsList(getDogs(searchString))
+
+  useEffect(() => {
+    changeDogsList(dogsData)
+  }, [dogsData])
+
+  return (
+    <Box
+      gridArea={'mainFilter'}
+      alignSelf='center'
+      justify={isSmall ? 'around' : 'end'}
+      background={'white'}
+      pad={'small'}
+    >
+      <Box>
+        <Text size={'medium'} margin={{right: 'large', top: 'medium', bottom: 'small'}}>
+          Пробанд
+        </Text>
+      </Box>
+      <FormField htmlFor='filter-input-id' margin={{top: 'small'}}>
+        <Select
+          id='filter-input-id'
+          name='Фильтр'
+          value={dogsList.find((dogData) => dogData._id === probandId)}
+          options={dogsList}
+          labelKey='fullName'
+          placeholder='Фильтр'
+          onSearch={(searchString) => handleSearch(searchString)}
+          onChange={(event) => changeProbandId(event.value._id)}
+        />
+      </FormField>
+    </Box>
+  )
+}
+
+export default ProbandSelect
