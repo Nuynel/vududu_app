167a687edd1cfcfa649cf85dcb0d7a9edf829bd6
alisa_kislova <alisa.k@plyo.io>
2024-08-01 13:31:34 +0200

    migrate-mongo new migration

--- apps/back/migrations/20240730155748-1_big_permissions_update.js
+++ apps/back/migrations/20240730155748-1_big_permissions_update.js
@@ -1,16 +1,114 @@
-import {DB_NAME} from "../src/constants";
+import {COLLECTIONS} from "../src/constants";
+import {DATA_GROUPS, DOG_TYPES, PERMISSION_GROUPS} from "../src/types";
+
+const permissions = {
+  viewers: {
+    [DATA_GROUPS.PUBLIC]: {
+      group: PERMISSION_GROUPS.REGISTERED,
+      profileIds: [],
+    },
+    [DATA_GROUPS.PROTECTED]: {
+      group: PERMISSION_GROUPS.ORGANISATION,
+      profileIds: [],
+    },
+    [DATA_GROUPS.PRIVATE]: {
+      group: null,
+      profileIds: [],
+    }
+  },
+  editors: {
+    [DATA_GROUPS.PUBLIC]: {
+      group: null,
+      profileIds: []
+    },
+    [DATA_GROUPS.PROTECTED]: {
+      group: null,
+      profileIds: []
+    },
+    [DATA_GROUPS.PRIVATE]: {
+      group: null,
+      profileIds: []
+    },
+  }
+}
 
 module.exports = {
   async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+    await db.collection(COLLECTIONS.DOGS).updateMany(
+      {},
+      {
+        $set: {
+          federationId: null,
+          healthCertificateIds: null,
+          permissions: permissions,
+        },
+        $unset: {
+          type: ''
+        }
+      }
+    );
+    await db.collection(COLLECTIONS.LITTERS).updateMany(
+      {},
+      {
+        $rename: { 'profileId': 'creatorProfileId' },
+        $set: {
+          federationId: null,
+          puppiesCount: {
+            male: null,
+            female: null
+          },
+          verified: {
+            status: false
+          }
+        }
+      }
+    );
+    await db.collection(COLLECTIONS.PROFILES).updateMany(
+      {},
+      {
+        $rename: { 'dogIds': 'ownDogIds' },
+        $set: {
+          otherDogIds: [],
+          otherLitterIds: []
+        }
+      }
+    );
   },
 
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    await db.collection(COLLECTIONS.DOGS).updateMany(
+      {},
+      {
+        $set: {
+          type: DOG_TYPES.MALE_DOG
+        },
+        $unset: {
+          federationId: null,
+          healthCertificateIds: null,
+          permissions: permissions,
+        }
+      }
+    );
+    await db.collection(COLLECTIONS.LITTERS).updateMany(
+      {},
+      {
+        $rename: { 'creatorProfileId': 'profileId' },
+        $unset: {
+          federationId: '',
+          puppiesCount: '',
+          verified: ''
+        }
+      }
+    );
+    await db.collection(COLLECTIONS.PROFILES).updateMany(
+      {},
+      {
+        $rename: { 'ownDogIds': 'dogIds' },
+        $unset: {
+          otherDogIds: '',
+          otherLitterIds: ''
+        }
+      }
+    );
   }
 };
